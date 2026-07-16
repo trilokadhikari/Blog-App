a64be05679fce0230b451deaad5ffd3176385e2f
trilokadhikari <tradhikari062@gmail.com>
2026-07-16 16:58:15 +0545

    Update configuration to include Appwrite environment variables

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,5 +1,9 @@
 const conf = {
-    // hidden items
+    appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APP_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APP_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APP_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APP_BUCKET_ID),
 
 }
 
